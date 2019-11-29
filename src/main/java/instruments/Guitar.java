0f303fe9c5b571ed689345a53cd19ef68b45f624
Cameron Moir <cameronjmoir@gmail.com>
2019-11-29 13:06:34 +0000

    can get guitar name

--- src/main/java/instruments/Guitar.java
+++ src/main/java/instruments/Guitar.java
@@ -1,4 +1,16 @@
 package instruments;
 
-public class Guitar {
+public class Guitar extends Instrument{
+
+    private int numberOfStrings;
+
+    public Guitar(String name, String material, String colour, String family, int purchasePrice, int salePrice,
+                    int numberOfStrings) {
+        super(name, material, colour, family, purchasePrice, salePrice);
+        this.numberOfStrings = numberOfStrings;
+    }
+
+    public int getNumberOfStrings(){
+        return numberOfStrings;
+    }
 }
--- src/test/java/GuitarTest.java
+++ src/test/java/GuitarTest.java
@@ -1,2 +1,22 @@
+import instruments.Guitar;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class GuitarTest {
+
+    Guitar guitar;
+
+    @Before
+    public void before(){
+        guitar = new Guitar("Fender", "oak", "red", "Strings", 100, 200,
+        4);
+    }
+
+    @Test
+    public void canGetName() {
+        assertEquals("Fender", guitar.getName());
+    }
+
 }
